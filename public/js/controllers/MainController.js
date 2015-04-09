9e0aaad10fa22f4059d480a08bb7bfdd7834d914
Sean Kim <nogever@gmail.com>
2015-04-09 18:39:32 -0400

    initial commit

---
+++ public/js/controllers/MainController.js
@@ -0,0 +1,46 @@
+app.controller('MainController', function ($scope, FlashCardsFactory) {
+    $scope.loaded = false;
+
+    $scope.flashCards = [];
+
+    $scope.categories = [
+        'MongoDB',
+        'Express',
+        'Angular',
+        'Node'
+    ];
+
+    $scope.chosenCategory = 'All';
+
+    $scope.getAllCards = function () {
+        $scope.chosenCategory = 'All';
+        FlashCardsFactory.getFlashCards().then(function (cards) {
+            $scope.flashCards = cards;
+            $scope.loaded = true;
+        });
+    };
+
+    $scope.getCategoryCards = function (category) {
+        $scope.chosenCategory = category;
+        FlashCardsFactory.getFlashCards(category).then(function (cards) {
+            $scope.flashCards = cards;
+        }).catch(function(err) {
+            $scope.error = err;
+        });
+    };
+
+    $scope.getAllCards();
+
+    $scope.cheatFilter = function() {
+        angular.forEach($scope.flashCards, function(flashCard) {
+            angular.forEach(flashCard.answers, function(answer) {
+                if (answer.correct) {
+                    answer.cheatFiltered = false;
+                } else {
+                    answer.cheatFiltered = true;
+                }
+            })
+        })
+    }
+
+});
